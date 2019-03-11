af1f7f90585e31e260eaeacac7e6246a6e965ae3
Marie Manandise <marie@studio24.net>
2019-03-11 14:09:26 +0000

    menu controller to fetch menus directly from twig templates

---
+++ src/Controller/MenuController.php
@@ -0,0 +1,42 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Psr\SimpleCache\CacheInterface;
+use Studio24\Frontend\Cms\Wordpress;
+use Studio24\Frontend\ContentModel\ContentModel;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+class MenuController extends AbstractController
+{
+    /**
+     * Frameworks Rest API data
+     *
+     * @var Wordpress
+     */
+    protected $api;
+
+    /**
+     * Generic menu controller
+     *
+     * @param integer $id
+     * @param string $templatePath
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function menu(int $id, string $templatePath = 'menus/default-menu.html.twig')
+    {
+        $this->api = new Wordpress(
+            getenv('APP_API_BASE_URL')
+        );
+
+        $menu = $this->api->getMenu($id);
+
+        return $this->render($templatePath, [
+            'menu' => $menu
+        ]);
+    }
+
+}
