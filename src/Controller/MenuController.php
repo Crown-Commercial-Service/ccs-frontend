6019b15a604cda64049ae339a4813d194a3bbcd3
Simon R Jones <simon@studio24.net>
2019-03-12 18:20:09 +0000

    Add cache to menu request

--- src/Controller/MenuController.php
+++ src/Controller/MenuController.php
@@ -19,6 +19,15 @@ class MenuController extends AbstractController
      */
     protected $api;
 
+    public function __construct(CacheInterface $cache)
+    {
+        $this->api = new Wordpress(
+            getenv('APP_API_BASE_URL')
+        );
+        $this->api->setCache($cache);
+    }
+
+
     /**
      * Generic menu controller
      *
@@ -28,10 +37,6 @@ class MenuController extends AbstractController
      */
     public function menu(int $id, string $templatePath = 'menus/default-menu.html.twig')
     {
-        $this->api = new Wordpress(
-            getenv('APP_API_BASE_URL')
-        );
-
         $menu = $this->api->getMenu($id);
 
         return $this->render($templatePath, [
