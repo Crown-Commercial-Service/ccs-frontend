8813b7e5748fdf8c80a886a7056dd22579258a8e
Matt Buckland <matt.b@studio24.net>
2019-03-14 13:56:46 +0000

    esource training data now contains times for the dates

--- src/Controller/FormController.php
+++ src/Controller/FormController.php
@@ -53,6 +53,9 @@ class FormController extends AbstractController
         $results = $this->api->getOne(0);
 
         $data = ['esourcingDates' => $results];
+        
+        print_r($data);
+        die();
 
         return $this->render('forms/31-esourcing-training.html.twig', $data);
     }
