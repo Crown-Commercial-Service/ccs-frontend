8c6361ee29b32338b36eef5600985e0e64466909
Dan Harris <dan@sparkcode.co.uk>
2019-10-29 15:22:40 +0000

    * Initial code

--- deploy/server_stop.sh
+++ deploy/server_stop.sh
@@ -3,7 +3,25 @@
 
 echo "Starting codedeploy server_stop.sh ..."
 
-sudo systemctl is-active --quiet httpd \
-    && sudo systemctl stop httpd.service
+SERVICES=(
+    "awslogsd.service"
+    "httpd.service"
+)
+
+# Service will not be installed on the first run
+echo "> Stopping services..."
+for SERVICE in "${SERVICES[@]}"; do
+    echo -n "> > Stopping service [$SERVICE]: "
+
+    SERVICE_REGEX=$(echo "$SERVICE" | sed -e 's/\./\\\\./g')
+
+    sudo systemctl list-unit-files "$SERVICE"|grep -q "^${SERVICE_REGEX}\\s"
+    if [ $? -eq 0 ]; then
+        sudo systemctl is-active --quiet "$SERVICE" \
+            && sudo systemctl stop "$SERVICE"
+    fi
+
+    echo "done."
+done
 
 echo "Codedeploy server_stop.sh complete."
