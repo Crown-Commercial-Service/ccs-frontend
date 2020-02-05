5a9b080f9638332f7392917c130582c406d11104
Simon R Jones <simon@studio24.net>
2020-02-05 17:22:48 +0000

    Ignore LineLength, make auto-fixes

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -6,7 +6,7 @@ require dirname(__DIR__) . '/vendor/autoload.php';
 
 // Load cached env vars if the .env.local.php file exists
 // Run "composer dump-env prod" to create it (requires symfony/flex >=1.2)
-if (is_array($env = @include dirname(__DIR__).'/.env.local.php')) {
+if (is_array($env = @include dirname(__DIR__) . '/.env.local.php')) {
     $_SERVER += $env;
     $_ENV += $env;
 } elseif (!class_exists(Dotenv::class)) {
