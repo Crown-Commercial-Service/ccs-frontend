5ce5b846c12e7748fc1f145171f78583cc87ac7d
Sean Dunwoody <sean.dunwoody@studio24.net>
2020-02-13 10:56:48 +0000

    Reduce cache to 2 minutes

--- src/Controller/WebinarController.php
+++ src/Controller/WebinarController.php
@@ -31,7 +31,7 @@ class WebinarController extends AbstractController
         );
         $this->api->setContentType('webinars');
         $this->api->setCache($cache);
-        $this->api->setCacheLifetime(300);
+        $this->api->setCacheLifetime(120);
     }
 
     public function request($id, $slug, Request $request)
