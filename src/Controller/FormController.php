01e69ab82b914d504c3cf4fa2d5af2e121cb097a
Simon R Jones <simon@studio24.net>
2020-03-06 18:40:05 +0000

    Testing Pardot Form Handler

--- src/Controller/FormController.php
+++ src/Controller/FormController.php
@@ -8,6 +8,9 @@ use Studio24\Frontend\ContentModel\ContentModel;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Psr\SimpleCache\CacheInterface;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use GuzzleHttp\Client;
 
 /**
  * Form controller
@@ -56,4 +59,53 @@ class FormController extends AbstractController
 
         return $this->render('forms/31-esourcing-training.html.twig', $data);
     }
+
+
+    /**
+     * Temporary form for testing Pardot
+     *
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \Studio24\Frontend\Exception\ContentFieldException
+     * @throws \Studio24\Frontend\Exception\ContentFieldNotSetException
+     * @throws \Studio24\Frontend\Exception\ContentTypeNotSetException
+     * @throws \Studio24\Frontend\Exception\FailedRequestException
+     * @throws \Studio24\Frontend\Exception\PermissionException
+     */
+    public function pardotTestGatedDownloadForm(Request $request)
+    {
+        $pardotFormUrl = 'https://go.pardot.com/l/811733/2020-03-06/263x3f';
+        $contentUrl = 'https://assets.crowncommercial.gov.uk/wp-content/uploads/Cloud-security-in-the-public-sector-1.pdf';
+
+        // Move this to a class in beta, see https://symfony.com/doc/current/forms.html
+        $form = $this->createFormBuilder()
+            ->add('email', EmailType::class)
+            ->add('submit', SubmitType::class, ['label' => 'Download content'])
+            ->getForm();
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Process form
+            $task = $form->getData();
+
+            // Submit to Pardot
+            $client = new Client();
+            $result = $client->request('POST', $pardotFormUrl, [
+                'form_params' => [
+                    'email' => $task['email'],
+                    'document' =>  parse_url($contentUrl, PHP_URL_PATH)
+                ]
+            ]);
+
+            // Could check return status is 200 via $result->getStatusCode()
+
+            return $this->redirect($contentUrl);
+        }
+
+        return $this->render('forms/pardot-test-gated-content.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
 }
