f73281e8eb215e599a00302cf9e039c924b2623e
Simon R Jones <simon@studio24.net>
2020-03-30 16:34:21 +0100

    Testing form submission

--- assets/scripts/pardot.js
+++ assets/scripts/pardot.js
@@ -1,9 +1,16 @@
+var pardotSubmitted = false;
+
 document.addEventListener("DOMContentLoaded", function (event) {
 
     /**
      * Send email address (and other data) to Pardot
      */
     $('form.pardot-submit').submit(function(event) {
+        if (pardotSubmitted) {
+            return true;
+        }
+
+        event.preventDefault();
 
         var email = $("form.pardot-submit input.pardot-email").val();
         if (email.length == 0) {
@@ -21,7 +28,11 @@ document.addEventListener("DOMContentLoaded", function (event) {
         $.ajax({
             url: "/api/pardot-email",
             method: "POST",
-            data: JSON.stringify(data)
+            data: JSON.stringify(data),
+            complete: function(){
+                pardotSubmitted = true;
+                $("form.pardot-submit").submit();
+            }
         });
     });
 
