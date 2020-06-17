295283b02684b13acca75140233bb8fe277eea0e
Alan Isaacson <38862429+AlanJIsaacson@users.noreply.github.com>
2020-06-17 12:19:14 +0100

    Update server_setup.sh
    Updated PHP install script

--- deploy/server_setup.sh
+++ deploy/server_setup.sh
@@ -43,20 +43,19 @@ if [ ! -e "$FIRST_RUN_PATH" ]; then
     sudo yum -y install https://centos7.iuscommunity.org/ius-release.rpm
 
     echo "> > Installing web packages..."
+    sudo amazon-linux-extras enable php7.3
     sudo yum -y install \
-        httpd \
-        mod_php73 \
-        php73-cli \
-        php73-mysqlnd.x86_64 \
-        php73-opcache \
-        php73-xml \
-        php73-gd \
-        php73-devel \
-        php73-intl \
-        php73-mbstring \
-        php73-bcmath \
-        php73-soap \
-        php73-json
+        php \
+        php-mysqlnd.x86_64 \
+        php-opcache.x86_64 \
+        php-xml.x86_64 \
+        php-gd.x86_64 \
+        php-devel.x86_64 \
+        php-intl.x86_64 \
+        php-mbstring.x86_64 \
+        php-bcmath.x86_64 \
+        php-soap.x86_64 \
+        php-json.x86_64
 
     echo "> > Moving httpd.conf..."
     sudo mv -f "$SCRIPTDIR/files/httpd.conf" /etc/httpd/conf/httpd.conf
