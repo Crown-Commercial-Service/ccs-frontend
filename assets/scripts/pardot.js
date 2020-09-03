947985987b9b0d99666c801c2094748f03a54ec4
Zubair Ali <zubair.ali@co-cc02842.ad.cabinetoffice.gov.uk>
2020-09-03 14:31:13 +0100

    add consent  wrapper to pardot.js

--- assets/scripts/pardot.js
+++ assets/scripts/pardot.js
@@ -1,43 +1,50 @@
+var cookiePreferences = JSON.parse(readCookie('cookie_preferences'));
 var pardotSubmitted = false;
 
-document.addEventListener("DOMContentLoaded", function (event) {
+// excute pardot JS if user consents to marketing cookies
+if (cookiePreferences !== null) {
+  if (cookiePreferences.marketing === true) {
+    document.addEventListener("DOMContentLoaded", function (event) {
 
-    /**
-     * Send email address (and other data) to Pardot
-     */
-    $('form.pardot-submit').submit(function(event) {
-        
-        if (pardotSubmitted) {
-            return true;
-        }
+        /**
+         * Send email address (and other data) to Pardot
+         */
+        $('form.pardot-submit').submit(function(event) {
 
-        console.log('Sending to Pardot.');
-
-        event.preventDefault();
-
-        var email = $("form.pardot-submit input.pardot-email").val();
-        if (email.length == 0) {
-            return;
-        }
-        var data = { "email": email };
-
-        var otherFields = $("input.pardot-field");
-        if (otherFields.length > 0) {
-            otherFields.each(function(index, element) {
-                data[$(this).attr('name')] = $(this).val();
-            });
-        }
-
-        $.ajax({
-            url: "/api/pardot-email",
-            method: "POST",
-            data: JSON.stringify(data),
-            complete: function(){
-                pardotSubmitted = true;
-                console.log('Proceeding with submission.');
-                $("form.pardot-submit button[type=submit]").click();
+            if (pardotSubmitted) {
+                return true;
             }
+
+            console.log('Sending to Pardot.');
+
+            event.preventDefault();
+
+            var email = $("form.pardot-submit input.pardot-email").val();
+            if (email.length == 0) {
+                return;
+            }
+            var data = { "email": email };
+
+            var otherFields = $("input.pardot-field");
+            if (otherFields.length > 0) {
+                otherFields.each(function(index, element) {
+                    data[$(this).attr('name')] = $(this).val();
+                });
+            }
+
+            $.ajax({
+                url: "/api/pardot-email",
+                method: "POST",
+                data: JSON.stringify(data),
+                complete: function(){
+                    pardotSubmitted = true;
+                    console.log('Proceeding with submission.');
+                    $("form.pardot-submit button[type=submit]").click();
+                }
+            });
         });
+
     });
 
-});
+    }
+}
