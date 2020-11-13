424dd4173c1ab024fe1a1044ea0e59eacac59a3b
Chee-Ng <58430495+Chee-Ng@users.noreply.github.com>
2020-11-13 10:27:51 +0000

    Revert "Update FrameworksController.php (#315)"
    This reverts commit 30a2ce046918d3d4670fb1271da2c92e7626b37d.

--- src/Controller/FrameworksController.php
+++ src/Controller/FrameworksController.php
@@ -379,8 +379,8 @@ class FrameworksController extends AbstractController
                 $guidedMatchStatusCode  = $guidedMatchResponse->getStatusCode();
 
                 try {
-                    // $responseForLandingPage = $this->guidedMatchApiClient->request('GET', getenv('GUIDED_MATCH_URL') . rawurlencode($query));
-                    $statusCodeForLandingPage = 200;
+                    $responseForLandingPage = $this->guidedMatchApiClient->request('GET', getenv('GUIDED_MATCH_URL') . rawurlencode($query));
+                    $statusCodeForLandingPage = $responseForLandingPage->getStatusCode();
                 } catch (ServerException $e) {
                     $statusCodeForLandingPage = 502;
                 }
