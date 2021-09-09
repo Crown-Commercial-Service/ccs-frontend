9e263583ac861cff78733700057536f71e66a4de
Bob <chee.ng@crowncommercial.gov.uk>
2021-09-09 11:39:09 +0100

    newsletter page will be in the codebase

---
+++ src/Helper/ControllerHelper.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Helper;
+
+class ControllerHelper
+{
+
+    public static function honeyPot($honeyPotField)
+    {
+        if (!empty($honeyPotField) && (bool) $honeyPotField == true) {
+            die;
+        }
+    }
+
+    public static function getOrgId()
+    {
+        return getenv('APP_ENV') === 'prod' ? getenv('ORG_ID_PROD') : getenv('ORG_ID_TEST');
+    }
+}
