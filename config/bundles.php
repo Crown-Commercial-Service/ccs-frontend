7ffbdbe470d40b7fdba0927f5a19e0bc329d799a
zubairali6 <zubair.ali@crowncommercial.gov.uk>
2023-01-19 15:09:32 +0000

    remove webserverbundler

--- config/bundles.php
+++ config/bundles.php
@@ -6,6 +6,5 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
-    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Rollbar\Symfony\RollbarBundle\RollbarBundle::class => ['all' => true],
 ];
