24506a35c60a45f2c5701bcc0909267bd28068d5
Chee-Ng <58430495+Chee-Ng@users.noreply.github.com>
2023-04-12 12:07:22 +0100

    whitespace

--- src/Validation/ContactCCSFormValidation.php
+++ src/Validation/ContactCCSFormValidation.php
@@ -50,7 +50,7 @@ class ContactCCSFormValidation
             'errors' => [],
             'link' => '#phone',
         ];
-        if ( !($callback == "No" || $callback == null) && empty(trim($phone))) {
+        if (!($callback == "No" || $callback == null) && empty(trim($phone))) {
             $returnArray['errors'] = ['Enter a telephone number, like 01632 960 001, 07700 900 982 or +44 0808 157 0192'];
         } elseif (preg_match("/[a-z]/i", $phone) || preg_match("/[*,!,#,$,%,^,&,?,<,>,=]/i", $phone)) {
             $returnArray['errors'] = ['Enter a telephone number in the correct format'];
