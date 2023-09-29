a821c5aed2b8ee3a3d0b35e33f5f2e85b1afea76
Chee-Ng <58430495+Chee-Ng@users.noreply.github.com>
2023-09-29 10:45:53 +0100

    fix GTM print bug on framework page

--- assets/scripts/dataLayer.js
+++ assets/scripts/dataLayer.js
@@ -12,6 +12,21 @@ window.onload = function() {
     }
 }
 
+//this is only ran on show framework and show supplier page
+function frameworkAndSupplierPage() {
+    const externalLinks = Array.from(document.querySelectorAll('main a')).filter(link => !(link.href.includes('crowncommercial') || link.href.includes('tel')) );
+
+    externalLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            pushToDataLayer({
+                "event":        'portal_click',
+                "link_text":    link.textContent.trim(),
+                "link_url":     link.href
+            });
+        });
+    });
+}
+
 function pushToDataLayer(array){
     array = (typeof array === 'string')? JSON.parse(array) : array;
 
