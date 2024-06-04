9fde8fd8770691d01ef03a5b83ec434c097ad62b
Chee-Ng <58430495+Chee-Ng@users.noreply.github.com>
2024-06-04 11:25:44 +0100

    correcting tests

--- tests/App/Utils/FrameworkCategoriesTest.php
+++ tests/App/Utils/FrameworkCategoriesTest.php
@@ -12,50 +12,58 @@ class FrameworkCategoriesTest extends WebTestCase
 {
     public function testFind()
     {
-        $cat = FrameworkCategories::find('People');
-        $this->assertEquals(4, count($cat));
+        $cat = FrameworkCategories::find('Corporate');
+        $this->assertEquals(5, count($cat['categories']));
+        $this->assertTrue(isset($cat['categories']));
+        
+        $cat = FrameworkCategories::find('Estates');
+        $this->assertEquals(4, count($cat['categories']));
+        $this->assertTrue(isset($cat['categories']));
+        
+        $cat = FrameworkCategories::find('Technology');
+        $this->assertEquals(5, count($cat['categories']));
         $this->assertTrue(isset($cat['categories']));
 
-        $cat = FrameworkCategories::find('Marcomms & Research');
-        $this->assertEquals('marcomms-research', $cat['slug']);
+        $cat = FrameworkCategories::find('HR and Workforce Services');
+        $this->assertEquals('hr-and-workforce-services', $cat['slug']);
         $this->assertFalse(isset($cat['categories']));
 
-        $cat = FrameworkCategories::find('Workplace');
-        $this->assertEquals('workplace', $cat['slug']);
+        $cat = FrameworkCategories::find('Estates Support Services');
+        $this->assertEquals('estates-support-services', $cat['slug']);
         $this->assertFalse(isset($cat['categories']));
 
-        $cat = FrameworkCategories::find('Below Threshold');
-        $this->assertEquals('below-threshold', $cat['slug']);
+        $cat = FrameworkCategories::find('Cloud and Hosting');
+        $this->assertEquals('cloud-and-hosting', $cat['slug']);
         $this->assertFalse(isset($cat['categories']));
 
-        $cat = FrameworkCategories::find('Fake');
+        $cat = FrameworkCategories::find('FakeCat');
         $this->assertNull($cat);
     }
 
     public function testCategories()
     {
         $categories = FrameworkCategories::getAll();
-        $this->assertEquals(20, count($categories));
+        $this->assertEquals(14, count($categories));
         $first = current($categories);
-        $this->assertEquals('below-threshold', $first);
+        $this->assertEquals('cloud-and-hosting', $first);
         $last = end($categories);
-        $this->assertEquals('workplace', $last);
+        $this->assertEquals('travel-accommodation-and-Venues', $last);
 
         $this->assertEquals('energy', FrameworkCategories::getSlug('Energy'));
-        $this->assertEquals('estates-decarbonisation', FrameworkCategories::getSlug('Estates Decarbonisation'));
-        $this->assertEquals('document-management-logistics', FrameworkCategories::getSlug('Document Management & Logistics'));
-        $this->assertEquals('Technology Products & Services', FrameworkCategories::getNameBySlug('technology-products-services'));
-        $this->assertEquals('Marcomms & Research', FrameworkCategories::getNameBySlug('marcomms-research'));
-        $this->assertEquals('Below Threshold', FrameworkCategories::getNameBySlug('below-threshold'));
-        $this->assertEquals(5, count(FrameworkCategories::getAllByPillar('People')));
+        $this->assertEquals('professional-services', FrameworkCategories::getSlug('Professional Services'));
+        $this->assertEquals('digital-capability-and-delivery', FrameworkCategories::getSlug('Digital Capability and Delivery'));
+        $this->assertEquals('Estates Support Services', FrameworkCategories::getNameBySlug('estates-support-services'));
+        $this->assertEquals('Facilities Management', FrameworkCategories::getNameBySlug('facilities-management'));
+        $this->assertEquals('Technology Services', FrameworkCategories::getNameBySlug('technology-services'));
+        $this->assertEquals(4, count(FrameworkCategories::getAllByPillar('Estates')));
     }
 
     public function testDbValue()
     {
-        $this->assertEquals('Buildings', FrameworkCategories::getDbValue('Buildings'));
-        $this->assertEquals('People', FrameworkCategories::getDbValue('People'));
-        $this->assertEquals('Marcomms & Research', FrameworkCategories::getDbValue('Marcomms & Research'));
-        $this->assertEquals('Below Threshold', FrameworkCategories::getDbValue('Below Threshold'));
+        $this->assertEquals('Corporate', FrameworkCategories::getDbValue('Corporate'));
+        $this->assertEquals('Travel, Accommodation and Venues', FrameworkCategories::getDbValue('Travel, Accommodation and Venues'));
+        $this->assertEquals('Facilities Management', FrameworkCategories::getDbValue('Facilities Management'));
+        $this->assertEquals('Technology', FrameworkCategories::getDbValue('Technology'));
     }
 
     public function testRedirectFromUtilitiesFuelsToEnergy()
@@ -64,7 +72,7 @@ class FrameworkCategoriesTest extends WebTestCase
         $client->request('GET', '/agreements/category/utilities-fuels');
         $response = $client->getResponse();
 
-        $this->assertEquals(302, $response->getStatusCode());
+        $this->assertEquals(301, $response->getStatusCode());
 
         $this->assertResponseRedirects('/agreements/category/energy');
     }
@@ -75,7 +83,7 @@ class FrameworkCategoriesTest extends WebTestCase
         $client->request('GET', '/agreements/category/travel');
         $response = $client->getResponse();
 
-        $this->assertEquals(302, $response->getStatusCode());
+        $this->assertEquals(301, $response->getStatusCode());
 
         $this->assertResponseRedirects('/agreements/category/travel-transport-accommodation-and-venues');
     }
@@ -86,19 +94,9 @@ class FrameworkCategoriesTest extends WebTestCase
         $client->request('GET', '/agreements/category/digital-future');
         $response = $client->getResponse();
 
-        $this->assertEquals(302, $response->getStatusCode());
+        $this->assertEquals(301, $response->getStatusCode());
 
         $this->assertResponseRedirects('/agreements/category/digital-specialists');
     }
 
-    public function testRedirectFromNetworkServicesToNetworkSolutions()
-    {
-        $client = static::createClient();
-        $client->request('GET', '/agreements/category/network-services');
-        $response = $client->getResponse();
-
-        $this->assertEquals(302, $response->getStatusCode());
-
-        $this->assertResponseRedirects('/agreements/category/network-solutions');
-    }
 }
