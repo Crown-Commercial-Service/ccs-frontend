7525495c02499606967e1a90c40354c0b8f397d5
Will Trust <will.trust@crowncommercial.gov.uk>
2025-06-04 16:15:20 +0100

    comment for stop

--- deploy/server_stop.sh
+++ deploy/server_stop.sh
@@ -10,22 +10,27 @@ SERVICES=(
 
 # Service will not be installed on the first run
 echo "> Stopping services..."
+echo "Step1: ${SERVICES[@]}"
 for SERVICE in "${SERVICES[@]}"; do
     echo -n "> > Stopping service [$SERVICE]: "
 
     SERVICE_REGEX=$(echo "$SERVICE" | sed -e 's/\./\\./g')
+    echo "Step2: under regex"
 
     #Temp Alpha Deploy Fix
     sudo yum reinstall httpd -y
     sudo systemctl start httpd
     sudo systemctl enable httpd
+    echo "Step3: under my commands"
 
     sudo systemctl list-unit-files "$SERVICE"|grep -q "^${SERVICE_REGEX}\\s"
+    echo "Step4: $SERVICE"|grep -q "^${SERVICE_REGEX}\\s"
     if [ $? -eq 0 ]; then
-
+        echo "Step5: Now in IF" 
         sudo systemctl is-active --quiet "$SERVICE" \
-            && echo "continuing just before stop."
+            && echo "continuing just before stop." #new
             && sudo systemctl stop "$SERVICE"
+            echo "Step6: Service Stopped" 
     fi
 
     echo "done."
