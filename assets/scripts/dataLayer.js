9527aa2c3fb9b5bfc7bbc61d3e20bdbfa2b8b88f
Jatin Patel <jatin.patel@crowncommercial.gov.uk>
2026-01-28 10:27:57 +0000

    Add missing functions to DataLayer.js

--- assets/scripts/dataLayer.js
+++ assets/scripts/dataLayer.js
@@ -144,19 +144,6 @@ function fileDownload( fileURL, fileName, fileSize) {
     });
 }
 
-function pushDataLayer(interaction_type, link_text, interaction_detail, step){
-    var env = document.getElementById('app-env').dataset.env;
-    if (env == "local" || env == "prod") {
-        window.dataLayer.push({
-            "event": 'gm_journey',
-            "interaction_type":     interaction_type !== null ? interaction_type : null,
-            "link_text":            link_text !== null ? link_text : null,
-            "interaction_detail":   interaction_detail !== null ? interaction_detail : null,
-            "step":                 step !== null ? step : null
-        });
-    }
-}
-
 function pushToDataLayer(array) {
     array = (typeof array === 'string') ? JSON.parse(array) : array;
     var env = document.getElementById('app-env').dataset.env;
@@ -181,4 +168,72 @@ function formatFileSize(bytes, decimals = 2) {
 function formatFileType(url) {
     const match = url.match(/\/([^\/?#]+)\.([a-zA-Z0-9]+)(?:[?#]|$)/);
     return match ? match[2] : ""
+}
+
+function pushDataLayer(interaction_type, link_text, interaction_detail, step){
+    var env = document.getElementById('app-env').dataset.env;
+    if (env == "local" || env == "prod") {
+        window.dataLayer.push({
+            "event": 'gm_journey',
+            "interaction_type":     interaction_type !== null ? interaction_type : null,
+            "link_text":            link_text !== null ? link_text : null,
+            "interaction_detail":   interaction_detail !== null ? interaction_detail : null,
+            "step":                 step !== null ? step : null
+        });
+    }
+}
+
+function questionType(type){
+    var answer = null;
+
+    switch(type){
+        case 'list':     
+            answer = list();
+            break;
+        case 'multiSelect':
+            answer = multiSelect();
+            break;
+        case 'boolean':
+            // we dont have any boolean quesiton on GM. They have all been archived.
+            break;
+    }
+
+    if(answer !== null){
+        pushDataLayer('continue','continue',null,answer);
+    }
+}
+
+function list(){
+    for (var i = 0; i < document.getElementsByClassName('v-selector').length; i++) {
+        if (document.getElementsByClassName('v-selector')[i].checked){
+            return document.getElementsByClassName('v-selector')[i].labels[0].textContent;
+        }
+    }
+    return null;
+}
+
+function multiSelect(){
+    var checkboxes = document.getElementsByName('uuid[]');
+    var checkboxesChecked = [];
+
+    for (var i=0; i<checkboxes.length; i++) {
+        if (checkboxes[i].checked) {
+            checkboxesChecked.push(checkboxes[i].labels[0].textContent);
+        }
+    }
+
+    return checkboxesChecked.join(' | ')
+}
+
+function printResultPage(){
+    pushDataLayer('print','Print this page', null, null);
+    window.print()
+}
+
+function contact(){
+    pushDataLayer('contact', 'Contact CCS', null, null);
+}
+
+function back(){
+    pushDataLayer('back', 'Back', null, null);
 }
